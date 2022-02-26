58d485d89cc0b06ec0e87c34e8ae41f3dbacf8d9
Alberth <alberthbernarth@gmail.com>
2022-02-26 13:41:13 -0400

    fetch

---
+++ src/bases/10-fetch.js
@@ -0,0 +1,15 @@
+const api_key = 'LD6WloCo8MSzyHjFsJOXuLsM1bXaGhP9';
+
+const peticion = fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ api_key }`);
+
+peticion
+    .then( resp => resp.json() )
+    .then( ({ data }) => {
+        const { url } = data.images.original;
+        
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append( img );
+    })
+    .catch( console.warn );
