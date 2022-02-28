7219556cd97fac05352ba7f9873cd601961f6fff
Alberth <alberthbernarth@gmail.com>
2022-02-28 18:23:25 -0400

    ternarios

---
+++ src/bases/12-ternario.js
@@ -0,0 +1,21 @@
+// Operador ternario
+
+const activo = true;
+
+// let mensaje;
+
+// if ( activo ) {
+//     mensaje = 'activo';
+// } else {
+//     mensaje = 'inactivo';
+// }
+
+
+// const mensaje = ( activo ) ? 'Activo' : 'mensaje';
+
+const mensaje = activo && 'Activo';
+
+console.log( mensaje );
+
+// en una condición AND si el primero es false entonces
+// la siguiente no se ejecuta.
